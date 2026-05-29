295533fa1de79015f36eff4a5aa42f29d2a1cc2c
Lila Bouzaza <lila.bouzaza@craftinmotion.fr>
2026-05-29 20:11:52 +0200

    refactor(architecture): removed procedural scripts

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-// On vide la session puis retour à l'accueil
-session_destroy();
-header('Location: index.php');
-exit;
--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer>
-    <p>© 2026 GrowMeet - Tous droits réservés</p>
-</footer>
-</body>
-</html>
